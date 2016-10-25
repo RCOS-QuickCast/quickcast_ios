a3e573d25fa996bdcf651bcac2458cab3b81bff2
Ethan Geng <jack@heffner-wl-421.dynamic.rpi.edu>
2016-10-25 17:11:26 -0400

    Initial Commit

---
+++ new QuickCast/MenuViewController.swift
@@ -0,0 +1,35 @@
+//
+//  MenuViewController.swift
+//  new QuickCast
+//
+//  Created by Ethan Geng on 21/10/2016.
+//  Copyright © 2016 Ethan. All rights reserved.
+//
+
+import UIKit
+
+class MenuViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.tableView.separatorStyle = UITableViewCellSeparatorStyle.none
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
