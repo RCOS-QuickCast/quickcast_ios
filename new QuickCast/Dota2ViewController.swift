459472ef8313adb5abf4df39a259941803642025
ethanrcos <jack@quad-wl-65.dynamic.rpi.edu>
2016-12-02 23:42:04 -0500

    implement the dota2 view controller

--- new QuickCast/Dota2ViewController.swift
+++ new QuickCast/Dota2ViewController.swift
@@ -11,6 +11,28 @@ import UIKit
 class Dota2ViewController: UIViewController {
 
     @IBOutlet weak var menuButton: UIBarButtonItem!
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // Return the number of rows in the section.
+        return Match.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cellIdentifier = "Cell"
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as! MatchTableViewCell
+        
+        cell.TeamImageView1.image = UIImage(named: "Dota2")
+        cell.TeamImageView2.image = UIImage(named: "Dota2")
+        // Configure the cell...
+        cell.TeamName1.text = TeamNames1[indexPath.row]
+        cell.TeamName2.text = TeamNames2[indexPath.row]
+        
+        return cell
+    }
+    
+    var Match = ["match 1", "match 2", "match 3", "match 4", "match 5", "match 6", "match 7", "match 8", "match 9", "match 10"]
+    var TeamNames1 = ["Team 1", "Team 3", "Team 5", "Team 7", "Team 9", "Team 11", "Team 13", "Team 15", "Team 17", "Team 19"]
+    var TeamNames2 = ["Team 2", "Team 4", "Team 6", "Team 8", "Team 10", "Team 12", "Team 14", "Team 16", "Team 18", "Team 20"]
+
     override func viewDidLoad() {
         super.viewDidLoad()
         if self.revealViewController() != nil {
